fa44d16dfc4690015ed5ca585ee9e856184e37f0
bobmoran47 <146392046+bobmoran47@users.noreply.github.com>
2023-10-15 18:43:02 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,35 +15,35 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_instance" "acme_corp" {
+resource "aws_instance" "acme" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.acme_corp.id]
+  vpc_security_group_ids = [aws_security_group.acme.id]
 
   tags = {
     Name = "ACME Corp Presentation"
   }
 }
 
-resource "aws_security_group" "acme_corp" {
-  name = "acme_corp"
+resource "aws_security_group" "acme" {
+  name = "acme"
   tags = {
     Terraform = "true"
   }
   vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_security_group_rule" "acme_corp_http_in" {
+resource "aws_security_group_rule" "acme_http_in" {
   type        = "ingress"
   from_port   = 80
   to_port     = 80
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.acme_corp.id
+  security_group_id = aws_security_group.acme.id
 }
 
 
-resource "aws_security_group_rule" "acme_corp_https_in" {
+resource "aws_security_group_rule" "acme_https_in" {
   type        = "ingress"
   from_port   = 443
   to_port     = 443
@@ -53,12 +53,12 @@ resource "aws_security_group_rule" "acme_corp_https_in" {
 }
 
 
-resource "aws_security_group_rule" "acme_corp_everything_out" {
+resource "aws_security_group_rule" "acme_everything_out" {
   type        = "egress"
   from_port   = 0
   to_port     = 0
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.acme.id
 }
    
