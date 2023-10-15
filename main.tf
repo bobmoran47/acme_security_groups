c4c95f2fd8fdc9282eabc8ff53ff7a36962fa969
bobmoran47 <146392046+bobmoran47@users.noreply.github.com>
2023-10-15 18:54:26 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,7 @@
- filter {
+data "aws_ami" "app_ami" {
+  most_recent = true
+
+  filter {
     name   = "name"
     values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
   }
@@ -18,47 +21,22 @@ data "aws_vpc" "default" {
 resource "aws_instance" "acme" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.acme.id]
+  vpc_security_group_ids = [module.acme_sg.security_group_id]
 
   tags = {
-    Name = "ACME Corp Presentation"
-  }
+    Name = "ACME Corp Presentation:
 }
 
-resource "aws_security_group" "acme" {
-  name = "acme"
-  tags = {
-    Terraform = "true"
-  }
-  vpc_id = data.aws_vpc.default.id
-}
+module "acme_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
 
-resource "aws_security_group_rule" "acme_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.acme.id
+  vpc_id  = data.aws_vpc.default.id
+  name    = "acme"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 
-resource "aws_security_group_rule" "acme_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.acme_corp.id
-}
-
-
-resource "aws_security_group_rule" "acme_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.acme.id
-}
-   
