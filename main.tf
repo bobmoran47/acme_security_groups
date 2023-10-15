064f3e5fa3c5cd50a865ae7cbc7b88088bef62c6
bobmoran47 <146392046+bobmoran47@users.noreply.github.com>
2023-10-15 17:30:21 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-filter {
+ filter {
     name   = "name"
     values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
   }
@@ -15,14 +15,50 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_instance" "acme" {
+resource "aws_instance" "acme_corp" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.acme.id]
+  vpc_security_group_ids = [aws_security_group.acme_corp.id]
 
   tags = {
-    Name = “ACME “Corp  Presentation”
+    Name = "ACME Corp Presentation"
   }
 }
 
-resource "aws_security_group" "ac
+resource "aws_security_group" "acme_corp" {
+  name = "acme_corp"
+  tags = {
+    Terraform = "true"
+  }
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "acme_corp_http_in" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.acme_corp.id
+}
+
+
+resource "aws_security_group_rule" "acme_corp_https_in" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.acme_corp.id
+}
+
+
+resource "aws_security_group_rule" "acme_corp_everything_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
+}
+   
